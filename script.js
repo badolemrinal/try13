387de6e0759f15157b67a3876908bb2406d3f5b9
badolemrinal <130122239+badolemrinal@users.noreply.github.com>
2026-02-06 15:28:14 +0530

    Updatee script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Are you sure babiee?",
+    "Really sure item??",
+    "Are you positive meri jaan?",
+    "Pratham please...",
+    "Just think about it gunda babiee!",
+    "If you say no, I will be really sad my babiee 😭...",
+    "I will be very sad 💔...",
+    "I will be very very very sad 💔...",
+    "Ok fine, I will stop asking pratham...",
+    "Just kidding, say yes please my babieeeee pleaseeeeee! ❤️"
 ];
 
 let messageIndex = 0;
